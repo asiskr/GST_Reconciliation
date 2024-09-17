5c992382b57fe0cb84b96109b15ecdead5c60597
unknown <asiskaur5678@gmail.com>
2024-09-17 09:51:08 +0530

    changed the pass

--- src/main/java/Pages/GenratingExcelPage.java
+++ src/main/java/Pages/GenratingExcelPage.java
@@ -119,7 +119,7 @@ public class GenratingExcelPage extends BaseClass {
 
 		// Then, send the Excel file as an email attachment
 		String from = "toptechautomation@theoutsourcepro.com.au";
-		String password = "Duz30077";
+		String password = "J7OJb*ZwQD25HpC2KO8*n";
 		String senderMail = from;
 
 		// Outlook SMTP server configuration
