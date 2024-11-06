90fd8d75d62f2dc60195d0518fd4803cb54b5bfa
Narsingh-841 <narsingh@theoutsourcepro.com.au>
2024-11-06 17:25:46 +0530

    order9

--- src/main/java/Pages/XeroGSTReconciliationPage.java
+++ src/main/java/Pages/XeroGSTReconciliationPage.java
@@ -1,73 +1,80 @@
 package Pages;
 
-import java.util.List;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import com.asis.util.BaseClass;
 import Driver_manager.DriverManager;
 
 public class XeroGSTReconciliationPage extends BaseClass {
 
+    @FindBy(xpath = "//button[contains(text(),'Accounting')]")
+    WebElement accountingButton;
 
-	@FindBy(xpath = "//button[contains(text(),'Accounting')]")
-	WebElement accountingButton;
-	@FindBy(xpath = "//a[contains(text(),'Reports')]")
-	WebElement reports;
-	@FindBy(xpath = "//*[@class='report-row-tooltip']//descendant::span[contains(text(),'GST Reconciliation')]")
-	WebElement GSTreconcil;
-	@FindBy(xpath = "//input[@id='fromDate']")
-	WebElement From;
-	@FindBy(xpath = "//input[@id='toDate']")
-	WebElement To;
-	@FindBy(xpath = "//a[@id='ext-gen27' and contains(@class, 'xbtn') and @href='javascript:']\n")
-	WebElement UpdateButton;
-	@FindBy(xpath = "//span[contains(text(),'Export')]")
-	WebElement export;
-	@FindBy(xpath = "//a[@title='Export to Excel']")
-	WebElement exportToExcel;
-	
-	// Constructor
-	public XeroGSTReconciliationPage() {	
-		PageFactory.initElements(DriverManager.getDriver(), this); 
-	}
-	//list  of all the actions on page
-	public static void getPageTitle() {
-	}
+    @FindBy(xpath = "//a[contains(text(),'Reports')]")
+    WebElement reports;
 
-	public void clickOnAccountingButton() {
-		wait.until(ExpectedConditions.elementToBeClickable(accountingButton));
-		accountingButton.click();
-		wait.until(ExpectedConditions.elementToBeClickable(reports));
-		reports.click();
-	}
-	public void clickOnGSTReconciliationButton() {
-		wait.until(ExpectedConditions.elementToBeClickable(GSTreconcil));
-		GSTreconcil.click();
-	}
-	public void clickExport() {
-		String StringFromDate = ATO_FROM_DATE;
-		wait.until(ExpectedConditions.elementToBeClickable(From));
-		From.clear();
-		From.sendKeys(StringFromDate);
-	}
-	public void clickOnExportToExcel() throws InterruptedException {
-		String StringToDate = ATO_TO_DATE;
-		wait.until(ExpectedConditions.elementToBeClickable(To));
-		To.clear();
-		To.sendKeys(StringToDate);
-		Thread.sleep(2000);
-		wait.until(ExpectedConditions.elementToBeClickable(UpdateButton));
-		UpdateButton.click();
-		js.executeScript("arguments[0].scrollIntoView(true)", export);
-		wait.until(ExpectedConditions.elementToBeClickable(export));
-		export.click();
-		wait.until(ExpectedConditions.elementToBeClickable(exportToExcel));
-		exportToExcel.click();
-		Thread.sleep(3000);
-		DriverManager.getDriver().quit();
-		
-	}
+    @FindBy(xpath = "//*[@class='report-row-tooltip']//descendant::span[contains(text(),'GST Reconciliation')]")
+    WebElement GSTreconcil;
+
+    @FindBy(xpath = "//input[@id='fromDate']")
+    WebElement From;
+
+    @FindBy(xpath = "//input[@id='toDate']")
+    WebElement To;
+
+    @FindBy(xpath = "//a[contains(@class, 'xbtn') and @href='javascript:']")
+    WebElement UpdateButton;
+
+    @FindBy(xpath = "//span[contains(text(),'Export')]")
+    WebElement export;
+
+    @FindBy(xpath = "//a[@title='Export to Excel']")
+    WebElement exportToExcel;
+
+    // Constructor
+    public XeroGSTReconciliationPage() {
+        PageFactory.initElements(DriverManager.getDriver(), this);
+    }
+
+    public static void getPageTitle() {
+        // Placeholder for any title-related actions if needed
+    }
+
+    public void clickOnAccountingButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(accountingButton)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(reports)).click();
+    }
+
+    public void clickOnGSTReconciliationButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(GSTreconcil)).click();
+    }
+
+    public void clickExport() {
+        String StringFromDate = ATO_FROM_DATE;
+        wait.until(ExpectedConditions.elementToBeClickable(From)).clear();
+        From.sendKeys(StringFromDate);
+    }
+
+    public void clickOnExportToExcel() throws InterruptedException {
+        String StringToDate = ATO_TO_DATE;
+        wait.until(ExpectedConditions.elementToBeClickable(To)).clear();
+        To.sendKeys(StringToDate);
+        Thread.sleep(2000); // Consider replacing this with explicit waits if possible
+
+        // Updated logic to handle the UpdateButton element
+        WebElement updateButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[contains(@class, 'xbtn') and @href='javascript:']")));
+        wait.until(ExpectedConditions.elementToBeClickable(updateButton)).click();
+
+        js.executeScript("arguments[0].scrollIntoView(true);", export);
+        wait.until(ExpectedConditions.elementToBeClickable(export)).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(exportToExcel)).click();
+        Thread.sleep(3000); // Consider replacing this with explicit waits if possible
+
+        DriverManager.getDriver().quit();
+    }
 }
